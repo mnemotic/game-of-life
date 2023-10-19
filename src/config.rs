d2d74ab12574a93ddb8d8cba372f94b9c57279d0
Martin Green <martin@bk2x.com>
2023-10-19 20:38:29 +0300

    Extract `config` module

---
+++ src/config.rs
@@ -0,0 +1,47 @@
+//
+// Copyright (c) 2023 Martin Green. All rights reserved.
+//
+
+pub mod window {
+    pub const WIDTH: u32 = 1280;
+    pub const HEIGHT: u32 = 720;
+}
+
+pub mod cells {
+    use bevy::math::Vec2;
+    use bevy::prelude::Color;
+
+    use once_cell::sync::Lazy;
+
+    use crate::color_gradient::{ColorGradient, ColorPoint};
+
+    pub const SPRITE_SIZE: Vec2 = Vec2::splat(20.0);
+    pub const SPRITE_WORLD_OFFSET: Vec2 = Vec2::new(10.0, 10.0);
+
+    pub const DEAD_COLOR: Color = Color::GRAY;
+
+    pub fn get_age_color(age: usize) -> Color {
+        // @REVIEW
+        //
+        // Replace with [`std::cell::LazyCell`](https://doc.rust-lang.org/std/cell/struct.LazyCell.html)
+        // once it drops in stable.
+
+        static GRADIENT: Lazy<ColorGradient> = Lazy::new(|| {
+            let mut gradient = ColorGradient::new();
+            gradient.insert(ColorPoint::new(0.0, Color::rgb_u8(139, 190, 28)));
+            gradient.insert(ColorPoint::new(0.2, Color::rgb_u8(162, 201, 38)));
+            gradient.insert(ColorPoint::new(0.4, Color::rgb_u8(185, 212, 47)));
+            gradient.insert(ColorPoint::new(0.6, Color::rgb_u8(209, 222, 57)));
+            gradient.insert(ColorPoint::new(0.8, Color::rgb_u8(232, 233, 66)));
+            gradient.insert(ColorPoint::new(1.0, Color::rgb_u8(255, 244, 76)));
+
+            gradient
+        });
+
+        GRADIENT.sample((age as f32) / 10.0)
+    }
+}
+
+pub mod sim {
+    pub const DEFAULT_TICKS_PER_SECOND: i32 = 4;
+}
--- src/main.rs
+++ src/main.rs
@@ -26,49 +26,8 @@ use input::ToggleCellTriggeredEvent;
 
 
 mod color_gradient;
+mod config;
 mod input;
-
-mod config {
-    pub mod window {
-        pub const WIDTH: u32 = 1280;
-        pub const HEIGHT: u32 = 720;
-    }
-
-    pub mod cells {
-        use bevy::math::Vec2;
-        use bevy::prelude::Color;
-
-        use once_cell::sync::Lazy;
-
-        use crate::color_gradient::{ColorGradient, ColorPoint};
-
-        pub const SPRITE_SIZE: Vec2 = Vec2::splat(20.0);
-        pub const SPRITE_WORLD_OFFSET: Vec2 = Vec2::new(10.0, 10.0);
-
-        pub const DEAD_COLOR: Color = Color::GRAY;
-
-        pub fn get_age_color(age: usize) -> Color {
-            // @REVIEW
-            //
-            // Replace with [`std::cell::LazyCell`](https://doc.rust-lang.org/std/cell/struct.LazyCell.html)
-            // once it drops in stable.
-
-            static GRADIENT: Lazy<ColorGradient> = Lazy::new(|| {
-                let mut gradient = ColorGradient::new();
-                gradient.insert(ColorPoint::new(0.0, Color::rgb_u8(139, 190, 28)));
-                gradient.insert(ColorPoint::new(0.2, Color::rgb_u8(162, 201, 38)));
-                gradient.insert(ColorPoint::new(0.4, Color::rgb_u8(185, 212, 47)));
-                gradient.insert(ColorPoint::new(0.6, Color::rgb_u8(209, 222, 57)));
-                gradient.insert(ColorPoint::new(0.8, Color::rgb_u8(232, 233, 66)));
-                gradient.insert(ColorPoint::new(1.0, Color::rgb_u8(255, 244, 76)));
-
-                gradient
-            });
-
-            GRADIENT.sample((age as f32) / 10.0)
-        }
-    }
-}
 
 
 const NEIGHBOR_OFFSETS: [IVec2; 8] = [
@@ -110,16 +69,18 @@ fn main() {
     let width = config::window::WIDTH;
     let height = config::window::HEIGHT;
 
-    let ticks_per_second = 2;
+    let tps = config::sim::DEFAULT_TICKS_PER_SECOND;
 
     App::new()
         .init_resource::<GameAssets>()
         .insert_resource(Msaa::Off)
         .insert_resource(ClearColor(Color::BLACK))
         .insert_resource(Life::new(width / 20, height / 20))
-        .insert_resource(SimulationConfig { ticks_per_second })
+        .insert_resource(SimulationConfig {
+            ticks_per_second: tps,
+        })
         .insert_resource(SimulationUpdateTimer(Timer::from_seconds(
-            1.0 / ticks_per_second as f32,
+            1.0 / tps as f32,
             TimerMode::Repeating,
         )))
         .add_state::<GameState>()
